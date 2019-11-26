a40d9b9f0b896548bf14a93f944c7d37a65cb24b
zfyangc <1107137440@qq.com>
2019-11-26 16:49:27 +0800

    aaaa

---
+++ apps/datafiling/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib import auth
+
+# Create your models here.
+
+class FilingRecord(models.Model):
+    pass
+
---
+++ apps/datafiling/serializer.py
@@ -0,0 +1,4 @@
+# coding:utf-8
+import django.core.serializers.base as serializers_base
+from rest_framework import serializers
+
